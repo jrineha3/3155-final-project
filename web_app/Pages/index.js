084c1c87385b22e899ca472491359c86b0c9f8b8
jrineha3 <jrineha3@uncc.edu>
2021-11-22 20:56:15 -0500

    issues with $(document).ready();

--- web_app/Pages/index.js
+++ web_app/Pages/index.js
@@ -1,1 +1,12 @@
-index
+$(document).ready(function() {
+    alert("ready");//alert to show that document is ready
+    let $submit = $('button');//creates jquery object containing all buttons
+    
+
+    $submit.each(function() {//runs function for each button
+        $(this).on('click', function(evt) {//adds event listener for mouse click event
+            evt.preventDefault();//prevents default action of submit button which submits the form
+            alert("click");//alert to show that a submit button has been clicked
+        });
+    });
+});
