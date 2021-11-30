21d943120a86d1c583d1ad7ed0642f6221ac30f8
jrineha3 <jrineha3@uncc.edu>
2021-11-30 18:49:22 -0500

    fixed ajax

--- web_app/Pages/index.js
+++ web_app/Pages/index.js
@@ -5,16 +5,18 @@ $(document).ready(function() {
     $navLink.each(function() {
         $(this).on('click', function(evt) {
             evt.preventDefault();
+
+            let $main = $("#index_main");
             //let page = this.attr('href');
+
             $.ajax({
-				type: "get",
-				url: "stats.json",
+				type: "GET",
+				url: "./stats.json",
 				dataType: "json",
 				success: function (data) {
-					let $main = $("index_main");
-					$main.html("");
-
-					$main.html(data.stats);
+                    let html = data.item;
+                    $main.html("");
+					$main.html(html);
 				},
 				error:   function (xhr, status, err) {
 					alert('something went wrong' + xhr.status + err);
@@ -25,23 +27,25 @@ $(document).ready(function() {
     
     $('#query_submit_1').on('click', function(evt) {
         evt.preventDefault();
-        let lat = $('#lat').val();
+        let $lat = $('#lat');
+        let $long = $('#long');
+        let lat = $lat.val();
         lat = lat.replace(/\s/g,'');
-        let long = $('#long').val();
+        let long = $long.val();
         long = long.replace(/\s/g,'');
 
-        if (isNaN(lat) ||isNaN(long)) {
+        if (isNaN(lat) || isNaN(long)) {
             alert("Please enter number values for lat and long!");
-            $('#lat').val("");
-            $('#long').val("");
+            $lat.val("");
+            $long.val("");
         }
         else if (!isNaN(lat) && !isNaN(long)) {
-            if (lat.length == '' || long.length == '') {
-                alert("Please enter a value for both lattitude and longitude!");
+            if (lat.length === '' || long.length === '') {
+                alert("Please enter a value for both latitude and longitude!");
             }
             
-            $('#lat').val(lat);
-            $('#long').val(long);
+            $lat.val(lat);
+            $long.val(long);
             $('#query_form_1').submit();
         }
 
@@ -49,22 +53,24 @@ $(document).ready(function() {
     });
 
     $('#query_submit_2').on('click', function(evt) {
-        evt.preventDefault();;
-        let city = $('#city').val
+        evt.preventDefault();
+        let $city = $('#city');
+        let city = $city.val();
         city.replace(/\s/g,'');
 
         if (!isNaN(city)) {
             alert("City names do not include numbers!");
         }
         else {
-            $('#city').val(city);
+            $city.val(city);
             $('#query_form_3').submit();
         }
     });
 
     $('#query_submit_3').on('click', function(evt) {
         evt.preventDefault();
-        let zip = $('#zip').val();
+        let $zip = $('zip');
+        let zip = $zip.val();
         zip = zip.replace(/\s/g,'');
 
         if (isNaN(zip)) {
@@ -72,14 +78,14 @@ $(document).ready(function() {
             $('#zip').val("");
         }
         else if (!isNaN(zip)) {
-            if (zip.length == '') {
+            if (zip.length === '') {
                 alert("Please enter a value!");
             }
             else if (zip.length < 5 || zip.length > 5) {
                 alert("ZIP code should be 5 digits!");
             }
             else {
-                $('#zip').val(zip);
+                $zip.val(zip);
                 $('#query_form_3').submit();
             }
         }
