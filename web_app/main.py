bea2b880e1ad47086d2b1d5dfafcbe98f2f1843a
Jonathan Painter <YourEmail@gmail.com>
2021-12-07 15:00:29 -0500

    update to main.py

--- web_app/main.py
+++ web_app/main.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, jsonify, redirect, send_file
 import pandas as pd
 from pandas import DataFrame, read_csv
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import io
+import base64
 
 app = Flask(__name__)
 
@@ -14,6 +16,7 @@ county21 = pd.read_csv('web_app/Datasets/annual_conc_by_monitor_2019.csv')
 monitor19 = pd.read_csv('web_app/Datasets/annual_aqi_by_county_2019.csv')
 monitor20 = pd.read_csv('web_app/Datasets/annual_aqi_by_county_2020.csv')
 monitor21 = pd.read_csv('web_app/Datasets/annual_aqi_by_county_2021.csv')
+data=pd.read_csv(csvfile)
 
 
 @app.route('/update_file', methods=['GET', 'POST'])
@@ -24,9 +27,8 @@ def update_file(request):
         lat = data.lat
         long = data.long
 
-    return render_template("stats.html")
-
-
+fig = go.Figure(data=data, layout=layout)
+pyo.plot(fig, filename='stats.html', auto_open=False)
 # main driver function
 
 if __name__ == '__main__':
