c02565dad603052063a635ecf744af64a5161958
Jonathan Painter <YourEmail@gmail.com>
2021-12-07 16:59:20 -0500

    ss

--- web_app/main.py
+++ web_app/main.py
@@ -7,29 +7,28 @@ import base64
 
 app = Flask(__name__)
 
-cbsa19 = pd.read_csv('web_app/Datasets/annual_aqi_by_cbsa_2019.csv')
-cbsa20 = pd.read_csv('web_app/Datasets/annual_aqi_by_cbsa_2020.csv')
-cbsa21 = pd.read_csv('web_app/Datasets/annual_aqi_by_cbsa_2021.csv')
+
 county19 = pd.read_csv('web_app/Datasets/annual_aqi_by_county_2019.csv')
 county20 = pd.read_csv('web_app/Datasets/annual_aqi_by_county_2020.csv')
-county21 = pd.read_csv('web_app/Datasets/annual_conc_by_monitor_2019.csv')
+county21 = pd.read_csv('web_app/Datasets/annual_aqi_by_county_2021.csv')
 monitor19 = pd.read_csv('web_app/Datasets/annual_aqi_by_county_2019.csv')
 monitor20 = pd.read_csv('web_app/Datasets/annual_aqi_by_county_2020.csv')
 monitor21 = pd.read_csv('web_app/Datasets/annual_aqi_by_county_2021.csv')
 data=pd.read_csv(csvfile)
 
 
-@app.route('/update_file', methods=['GET', 'POST'])
+@app.route('/index.html', methods=['GET', 'POST'])
 def update_file(request):
-    if request.is_ajax():
-        data = request.POST.get('form_data')
 
-        lat = data.lat
-        long = data.long
+        with open('web_app/Datasets/annual_aqi_by_county_2019.csv', 'r') as csvfile:
+            lines = csv.reader(csvfile, delimiter=',')
+            for row in lines:
+                Names.append(row[0])
+                Values.append(int(row[1]))
+        redirect('barchart.html')
 
-fig = go.Figure(data=data, layout=layout)
-pyo.plot(fig, filename='stats.html', auto_open=False)
-# main driver function
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
