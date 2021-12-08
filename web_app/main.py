50739d6cba2a137d0f94db97af0df7e5406410fd
Jonathan Painter <YourEmail@gmail.com>
2021-12-08 15:55:24 -0500

    Update to main.py and chromomap

--- web_app/main.py
+++ web_app/main.py
@@ -26,69 +26,48 @@ combined_csv_County.to_csv( "county.csv", index=False, encoding='utf-8-sig')
 
 #monitor19 = pd.read_csv('web_app/Datasets/annual_conc_by_monitor_2019.csv')
 #monitor20 = pd.read_csv('web_app/Datasets/annual_conc_by_monitor_2020.csv')
-#monitor21 = pd.read_csv('web_app/Datasets/annual_conc_by_monitor_2021.csv')
-os.chdir("web_app/Datasets/Monitor")
-extensionMonitor = 'csv'
-all_filenames_Monitor = [i for i in glob.glob('*.{}'.format(extensionMonitor))]
-#combine all files in the list
-combined_csv_Monitor = pd.concat([pd.read_csv(f) for f in all_filenames_Monitor ])
-#export to csv
-combined_csv_Monitor.to_csv( "monitor.csv", index=False, encoding='utf-8-sig')
+
+
 
 
 @app.route('./index.html', methods=['GET', 'POST'])
 def barchart(request):
+        df = pd.read_csv('web_app/Datasets/County/annual_aqi_by_county_2019.csv')
+        form=request.form
+        for field in form:
+         if field.has_key('state'):
+                filtered_df= df["State"]
+                new_df =  filtered_df.groupby(['State'])['Median AQI'].sum().reset_index()
+                data = [go.Bar(x=new_df['State name'], y=new_df['Median AQI'])]
+                layout = go.Layout(title='Average AQI 2021', xaxis_title="State",
+                yaxis_title="AQI Value")            
+                fig = go.Figure(data=data, layout=layout)
+                pyo.plot(fig, filename='barchart.html', auto_open=False)
+        
+         elif  field.has_key('county'): #if user enters filter by county
+                filtered_df= df["County"]
+                new_df =  filtered_df.groupby(['County'])['Median AQI'].sum().reset_index()
+                data = [go.Bar(x=new_df['State name'], y=new_df['Median AQI'])]
+                layout = go.Layout(title='Average AQI 2021', xaxis_title="State",
+                yaxis_title="AQI Value")            
+                fig = go.Figure(data=data, layout=layout)
+                pyo.plot(fig, filename='barchart.html', auto_open=False)
 
-        df = pd.read_csv(combined_csv_County)
+         elif  field.has_key('aqi'): # if user enters by median AQI
+                filtered_df= df["Median AQI"]
+                new_df =  filtered_df.groupby(['State name'])['Median AQI'].sum().reset_index()
+                data = [go.Bar(x=new_df['State name'], y=new_df['Median AQI'])]
+                layout = go.Layout(title='Average AQI 2021', xaxis_title="State",
+                yaxis_title="AQI Value")            
+                fig = go.Figure(data=data, layout=layout)
+                pyo.plot(fig, filename='barchart.html', auto_open=False)
 
-        filtered_df= df["Years"]
-        # Removing empty spaces from State column to avoid errors
-        filtered_df = filtered_df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
-        # Creating sum of number of cases group by State Column
-        new_df =  filtered_df.groupby(['Years'])['90th Percentile AQI'].sum().reset_index()
-        # Sorting values and select first 20 states
-        new_df = new_df.sort_values(by=['90th Percentile AQI'], ascending=[True]).head(20)
-        # Preparing data
-        data = [go.Bar(x=new_df['Years'], y=new_df['90th Percentile AQI'])]
-        # Preparing layout
-        layout = go.Layout(title='Average AQI by Years', xaxis_title="Years",
-                   yaxis_title="AQI Value")            
-        # Plot the figure and saving in a html file
-        fig = go.Figure(data=data, layout=layout)
-        pyo.plot(fig, filename='barchart.html', auto_open=False)
+
         return redirect("./barchart.html")
     
 
-
-@app.route('./index.html', methods=['GET', 'POST'])
-def chloropleth(request):
-            df = pd.read_csv(combined_csv_Monitor)
-            data = dict(type='choropleth',
-            # location: Arizoana, California, Newyork
-            locations=['AZ', 'CA', 'NY'],
-
-
-            # States of USA
-            locationmode='USA-states',
-
-            # colorscale can be added as per requirement
-            colorscale='Reds',
-
-            # text can be given anything you like
-            text=['2019', '2020', '2021'],
-            z=[1.0, 2.0, 3.0],
-            colorbar={'title': 'AQI levels'})
-
-            layout = dict(geo={'scope': 'usa'})
-
-            # passing data dictionary as a list
-            choromap = go.Figure(data=[data], layout=layout)
-
-
-
-            # Plot the figure and saving in a html file
-            pyo.plot(choromap, filename='choromap.html', auto_open=False)
-            return redirect("./choropleth.html")
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+
+
+
--- web_app/plot-templates/choromap.py
+++ web_app/plot-templates/choromap.py
@@ -7,36 +7,38 @@ import glob
 # type defined is choropleth to
 # plot geographical plots
 
-os.chdir("web_app/Datasets/County")
-extensionCounty = 'csv'
-all_filenames_County = [i for i in glob.glob('*.{}'.format(extensionCounty))]
-#combine all files in the list
-combined_csv_County = pd.concat([pd.read_csv(f) for f in all_filenames_County ])
-#export to csv
-combined_csv_County.to_csv( "county.csv", index=False, encoding='utf-8-sig')
-df = pd.read_csv(combined_csv_County)
+
+df = pd.read_csv('web_app/Datasets/County/annual_aqi_by_county_2019.csv')
+
 
 data = dict(type='choropleth',
 
             # location: Arizoana, California, Newyork
-            locations=['AZ', 'CA', 'NY'],
+            locations=["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA","HI", "ID","IL","IN",
+                        "IA","KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV","NH", "NJ",
+                        "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT",
+                        "VT", "VA", "WA", "WV", "WI", "WY"],
 
             # States of USA
             locationmode='USA-states',
 
             # colorscale can be added as per requirement
-            colorscale='Portland',
+            colorscale='Reds',
+            autocolorscale=False,
+            reversescale=False,
 
             # text can be given anything you like
             text=['text 1', 'text 2', 'text 3'],
-            z=df['90th Percentile AQI'],
+            z=df['Median AQI'],
             colorbar={'title': 'AQI Levels'})
 
 layout = dict(geo={'scope': 'usa'})
 
 # passing data dictionary as a list
 choromap = go.Figure(data=[data], layout=layout)
+choromap.show()
+
 
 
 # Plot the figure and saving in a html file
-pyo.plot(choromap, filename='choromap.html', auto_open=False)
+#pyo.plot(choromap, filename='choromap.html', auto_open=False)
